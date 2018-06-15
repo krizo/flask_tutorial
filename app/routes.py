e90ec444ea6374bd25bbe8f27db9510e3a6a4314
Krzysztof Bober <krzysztof.bober@getbase.com>
2018-06-15 23:45:11 +0200

    Introducing templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World!'
+    user = { 'username': 'Kriz' }
+    posts = [
+        {
+            'author': user,
+            'body': 'Yay, first blog entry'
+        },
+        {
+            'author': { 'username': 'Susan' },
+            'body': 'And the second one'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
